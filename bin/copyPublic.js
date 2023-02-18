eb6d1f64dbe7d4c27624317a27ac8a6f41f9caa7
Saeid Mohadjer <saeid@fastmail.fm>
2023-02-18 15:42:59 +0100

    updates watch for .ts files

--- bin/copyPublic.js
+++ bin/copyPublic.js
@@ -1,6 +1,7 @@
 const config = require('./config.js');
 const fse = require("fs-extra");
 const path = require('path');
+const utils = require('./utils.js');
 
 function copyDependencies(type) {
   if (config.dependencies[type]) {
@@ -22,14 +23,35 @@ function copyResources(folder) {
 	const srcDir = './app/resources/' + folder;
 	const destDir = './public/resources/' + folder;
 
-	//https://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
-	try {
-		fse.copySync(srcDir, destDir, {overwrite: true});
-		console.log(srcDir, 'was copied to ', destDir);
-	}
-	catch (err) {
-		console.error('error: ', err);
-	}
+  if (folder === 'js') {
+    //compile typescript files
+    utils.traverseDir(srcDir, (filePath) => {
+      console.log('js: ', filePath);
+      const extension = path.extname(filePath);
+      console.log(extension);
+
+      // typescript files are compiled and copied via tsc compiler in npm script
+      if (extension !== '.ts') {
+        const targetPath = filePath.replace('app/', 'public/');
+        fse.copy(filePath, targetPath, function (err) {
+          if (err){
+            console.log('An error occured while copying the folder.')
+            return console.error(err)
+          }
+          console.log(targetPath, ' copy completed!')
+        });
+      }
+    });
+  } else {
+    //https://stackoverflow.com/questions/13786160/copy-folder-recursively-in-node-js
+    try {
+      fse.copySync(srcDir, destDir, {overwrite: true});
+      console.log(srcDir, 'was copied to ', destDir);
+    }
+    catch (err) {
+      console.error('error: ', err);
+    }
+  }
 }
 
 function copyAssets() {
