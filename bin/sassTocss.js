e2bcf92db41edffae2b602a5a3e27da5c1745cab
Saeid Mohadjer <saeid@fastmail.fm>
2023-04-13 22:20:48 +0200

    fix for sass task

--- bin/sassTocss.js
+++ bin/sassTocss.js
@@ -1,18 +1,38 @@
-var fs = require('fs');
-var sass = require('sass');
+const fs = require('fs');
+const path = require('path');
+const sass = require('sass');
+const dir = 'app/resources/css';
+const targetDir = dir.replace('app/', 'public/');
+const convertToCSS = (fullPath, targetPath) => {
+  console.log('converting ', fullPath, ' to css...');
+  sass.render({
+    file: fullPath,
+    //includePaths: ['./app/resources/css/modules/']
+  }, function(err, result) {
+    if(!err) {
+        // No errors during the compilation, write this result on the disk
+        if (!fs.existsSync(targetDir)){
+          fs.mkdirSync(targetDir, { recursive: true });
+        }
 
-sass.render({
-  file: './app/resources/css/styles.scss',
-  includePaths: ['./app/resources/css/modules/']
-}, function(err, result) {
-  if(!err) {
-      // No errors during the compilation, write this result on the disk
-      fs.writeFile('public/resources/css/styles.css', result.css, function(err){
-      if(!err){
-        //file written on disk
-      }
-      });
-  } else {
-    console.log(err);
+        fs.writeFile(targetPath, result.css, function(err){
+          if(!err){
+            //file written on disk
+          }
+        });
+    } else {
+      console.error(err);
+    }
+  });
+};
+
+fs.readdirSync(dir).forEach(file => {
+  const fullPath = path.join(dir, file);
+  const targetPath = fullPath.replace('app/', 'public/');
+  const targetCSS = targetPath.replace('.scss', '.css');
+
+  // to exclude modules folder
+  if (!fs.lstatSync(fullPath).isDirectory()) {
+    convertToCSS(fullPath, targetCSS);
   }
 });
